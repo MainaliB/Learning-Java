d11ece38d64c0a66cdb40ef2d1d0cd331a14007e
Bibek Mainali <bibek.mainali20@gmail.com>
2022-02-18 15:22:14 -0500

    Count Positive

---
+++ CountPositive.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bibekmainali
+ */
+import java.util.Scanner;
+public class CountPositive {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter an integer, the inpur ends if it is O: ");
+        String numString = input.next();
+        
+        while (numArr != 0){
+            }
+        }
+    
+}
