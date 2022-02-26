61c1e23af7541db225e1f8d07a228b946fd23622
Bibek Mainali <bibek.mainali20@gmail.com>
2022-02-25 21:17:32 -0500

    Program to display pattern

---
+++ DsipalyPattern.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bibekmainali
+ */
+public class DsipalyPattern {
+    public static void main(String[] args){
+        System.out.println("    J     A    V      V   A");
+        System.out.println("    J    A  A    V   V   A  A");
+        System.out.println("J   J   AAAAAA    V  V  AAAAAA");
+        System.out.println(" JJ    A      A     V  A      A");
+        
+    }
+    
+}
