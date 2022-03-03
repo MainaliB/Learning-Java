89ec23c9d7eb21f4785edb7466cebdc70a663946
Bibek Mainali <bibek.mainali20@gmail.com>
2022-03-02 20:46:25 -0500

    Program to find the largest element in a 2-d Array and its row and col index

--- Location.java
+++ Location.java
@@ -9,8 +9,8 @@
  */
 import java.util.*;
 public class Location {
-    int row;
-    int col;
+    int rowIndex;
+    int colIndex;
     double maxValue;
     
     //main method
@@ -31,63 +31,46 @@ public class Location {
     
             }
         }
+        //create a new location object ysing the method locateLarges which returns a location object
+        Location location2 = locateLargest(numbersArray);
+        //Print out the output
+        System.out.println("The largest value "+ location2.maxValue + " is located in "
+       +"<"+ location2.rowIndex + "," + location2.colIndex + ">.");
+        
     }    
     
     
-   //constructor
-    public Location(int row, int col, double maxValue){
-        this.row = row;
-        this.col = col;
+   // constructor
+    public Location(double maxValue, int rowIndex, int colIndex){
         this.maxValue = maxValue;
+        this.rowIndex = rowIndex;
+        this.colIndex = colIndex;
     }
-    
-    
-    
-    //method to return the index of the largest value in a 2 dimensional array
-    public static int[] locateLargest(double [][]a){
-        
+    //method to locate the largest element in the array, returns a location object
+    public static Location locateLargest(double [][]a){
         double max = a[0][0];
+        int rowIndex=0;
+        int colIndex=0;
         
-        int [] indexOfMax = new int[2];
-        indexOfMax[0]=0;
-        indexOfMax[1]=0;
+        //int [] indexOfMax = new int[2];
+        //indexOfMax[0]=0;
+        //indexOfMax[1]=0;
         for(int i = 0; i < a.length; i++){
             for (int j = 0; j < a[i].length; j++){
                 if (a[i][j]>max){
-                    indexOfMax[0] = i;
-                    indexOfMax[1] = j;
+                    rowIndex= i;
+                    colIndex = j;
                     max = a[i][j];
                 }
                
                         
                 }
             }
-        
-        
-        return indexOfMax;
-    }
-    //method to return the largest value in the array
-    public static double largestValue(double [][]a){
-        
-        double max = a[0][0];
-        
-        int [] indexOfMax = new int[2];
-        indexOfMax[0]=0;
-        indexOfMax[1]=0;
-        for(int i = 0; i < a.length; i++){
-            for (int j = 0; j < a[i].length; j++){
-                if (a[i][j]>max){
-                    indexOfMax[0] = i;
-                    indexOfMax[1] = j;
-                    max = a[i][j];
-                }
-               
-                        
-                }
-            }
-        
-        
-        return max;
-    }
+        Location location1 = new Location(max, rowIndex, colIndex);
+    return location1;
     
+    }
+     
+    
+           
 }
