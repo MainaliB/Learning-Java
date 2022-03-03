fdc354ca9c751c11dc584dd54f8870b04a8bbbd5
Bibek Mainali <bibek.mainali20@gmail.com>
2022-03-02 22:01:00 -0500

    Program to calculate outstanding balance after n number of months in a car loan

---
+++ OutstandingBalance.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bibekmainali
+ */
+//create a program that allows user to input their car loan information and calcuate an outstanding balance after n number of months
+//work in progress
+public class OutstandingBalance {
+    double loanAmount;
+    double annualInterestRate;
+    double monthlyPayment;
+    int numberOfMonths;
+    double outstandingBal;
+    
+    
+    //constructor
+   public OutstandingBalance(double loanAmount, double annualInterestRate, double monthlyPayment, int numberOfMonths, double outstandingBal){
+       this.loanAmount = loanAmount;
+       this.annualInterestRate= annualInterestRate;
+       this.monthlyPayment= monthlyPayment;
+       this.numberOfMonths= numberOfMonths;
+       this.outstandingBal= outstandingBal;
+   
+   }
+   
+   public double getOutstangingBal(){
+   
+   
+   }
+    
+}
