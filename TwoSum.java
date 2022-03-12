5a0ec09578f8f768c07c6bba8a41f33410428a81
Bibek Mainali <bibek.mainali20@gmail.com>
2022-03-11 21:24:00 -0500

    Work in Progress

---
+++ TwoSum.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bibekmainali
+ */
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+*/
+public class TwoSum {
+    public static int[] getIndex(int[] a, int targer){
+    int [] outputIndex = new [2];
+    for (int i = 0; i < a.length; i++){
+        //work in progress
+    }
+    
+    
+    
+    }
+    
+}
