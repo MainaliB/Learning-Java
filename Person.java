8556e793f149a313c2c7276b895a3b63d6f6a8c0
Bibek Mainali <bibek.mainali20@gmail.com>
2022-03-12 22:06:25 -0500

    Final Exam Problem

---
+++ Person.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bibekmainali
+ */
+public class Person {
+    private String name;
+
+ 
+//constructor
+    public Person( ) {
+
+        name = "Unknown";
+
+    }
+
+ 
+//method to get name
+    public String getName() {
+
+        return name;
+
+    }
+
+ 
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+}
+
+
+    
+
