4acbc4e81531a8a1ae445d794f702ae9ef02d3e2
Bibek Mainali <bibek.mainali20@gmail.com>
2022-03-14 21:35:46 -0400

    Work in Progress

--- TwoSum.java
+++ TwoSum.java
@@ -14,14 +14,42 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
 
 */
+import java.util.*;
 public class TwoSum {
-    public static int[] getIndex(int[] a, int targer){
-    int [] outputIndex = new [2];
-    for (int i = 0; i < a.length; i++){
-        //work in progress
+    
+    public static void main(String[] args){
+    System.out.println("Please enter the length for the array: ");
+    Scanner input = new Scanner(System.in);
+    int lenArray = input.nextInt();
+    
+    int [] arrayA = new int [lenArray];
+    System.out.println("Please enter the numbers for array: ");
+    for (int i = 0; i < lenArray; i ++){
+        arrayA[i] = input.nextInt();
+    
     }
     
+    System.out.println("Please enter the target value: ");
+    int target = input.nextInt();
     
+    System.out.println(getIndex(arrayA, target));
+    }
+    public static int[] getIndex(int[] a, int target){
+    int [] outputIndex = new int [2];
+    for (int i = 0; i < a.length; i++){
+        int numToFind = target - a[i];
+        for (int j = i+1; j < a.length; j++){
+            if (j == numToFind){
+                outputIndex[0] = i;
+                outputIndex[1] = j;
+            
+            }
+              
+                }
+        //work in progress
+    }
+   
+    return outputIndex;
     
     }
     
