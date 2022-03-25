3af64b467a807db8483a8e92c6b36372aa292f83
Bibek Mainali <bibek.mainali20@gmail.com>
2022-03-24 23:10:58 -0400

    Work in Progress

---
+++ Problem04.java
@@ -0,0 +1,18 @@
+/*
+Write a program that creates an array of length 25, and fills it with Fibonacci 
+sequence until the array is full. Once the array is filled print out each item 
+of the array (all items on one line, separated by a space).
+
+The Fibonacci sequence is the series of numbers where each number is the sum of 
+the two preceding numbers.
+ */
+public class Problem04 {
+      public static void main(String[] args) {
+        fillWithFibonacci();
+    }
+    
+    public static void fillWithFibonacci() {
+        // Your code here
+    }
+    
+}
