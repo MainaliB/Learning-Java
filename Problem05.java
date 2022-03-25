ca4f66114110df72e8453c74fde546dd5fee0661
Bibek Mainali <bibek.mainali20@gmail.com>
2022-03-24 23:11:13 -0400

    Work In Progress

---
+++ Problem05.java
@@ -0,0 +1,16 @@
+/*
+Write a method that accepts an array of ints that has been filled with a 
+random number between 1-10 at each index. Your method should count the number
+of times that the number 7 appears in the array.
+ */
+public class Problem05 {
+     public static void main(String[] args) {
+        int myArray[] = {1,2,3,4,5,6,7,8,9,10,7,7,7};
+        System.out.println(countSevens(myArray));
+    }
+
+    public static int countSevens(int[] myArray) {
+      // Your code here
+    }
+    
+}
