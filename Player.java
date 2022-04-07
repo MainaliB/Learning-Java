48182568c4d9f67d197bdfa2b55c78b6c8adc9bb
Bibek Mainali <bibek.mainali20@gmail.com>
2022-04-06 22:24:57 -0400

    Work in Progress

---
+++ Player.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bibekmainali
+ */
+public class Player {
+    private int Health;
+    private int Intelligence;
+    private int Dexterity;
+    private int Strength;
+    private int Stamina;
+    
+    
+    public Player(){
+    
+    }
+    
+    public Player(int health, int intelligence, int dexterity, int strength, int stamina){
+        this.Health= health;
+        this.Intelligence = intelligence;
+        this.Dexterity = dexterity;
+        this.Strength = strength;
+        this.Stamina = stamina;
+    }
+    
+    public int getHealth(){
+        return Health;
+    } 
+    
+    public void setHealth(int number){
+        if (number <0){
+            System.out.println("You can not set a value less than 0. Try again!");
+        }
+        else{
+        Health = number;
+        }
+    }
+    
+    public int getIntelligence(){
+        return Intelligence;
+    }
+    
+    public void setIntelligence(int number){
+        if (number <0){
+            System.out.println("You can not set a value less than 0. Try again!");
+        }
+        else{
+        Intelligence = number;
+        }
+    }
+    
+    public int getDexterity(){
+        return Dexterity;
+    }
+    
+    public void setDexterity(int number){
+        if (number <0){
+            System.out.println("You can not set a value less than 0. Try again!");
+        }
+        else{
+        Dexterity = number;
+        }
+    }
+    
+    
+    public int getStrength(){
+        return Strength;
+    }
+    
+    public void setStrength(int number){
+        if (number <0){
+            System.out.println("You can not set a value less than 0. Try again!");
+        }
+        else{
+        Strength= number;
+        }
+    }
+    
+    public int getStamina(){
+        return Stamina;
+    }
+    
+    public void setStamina(int number){
+        if (number <0){
+            System.out.println("You can not set a value less than 0. Try again!");
+        }
+        else{
+        Stamina= number;
+        }
+    }
+    
+    public String toString(){
+        return Health + " " + Intelligence + " " + Dexterity + " " + Strength + " "+ Stamina;
+    }
+    
+    
+}
