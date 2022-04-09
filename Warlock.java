32bc6a4ddea155a646856a40eb0c4a592585d4ae
Bibek Mainali <bibek.mainali20@gmail.com>
2022-04-08 22:51:25 -0400

    work in progress

---
+++ Warlock.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bibekmainali
+ */
+public class Warlock extends Player{
+    
+}
